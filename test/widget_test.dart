90f1bcb4ac03e8c6d76a03718727276772c3ba8f
Ricardo Oliveira <ricardooliver.ads.2019@gmail.com>
2026-01-24 17:26:11 -0400

    style: format code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.   
+    // Build our app and trigger a frame.
   });
 }
