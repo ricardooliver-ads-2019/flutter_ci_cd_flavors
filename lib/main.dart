019b130c3c5e5712c9057296df199ac09b5524ae
Ricardo Oliveira <ricardooliver.ads.2019@gmail.com>
2026-01-25 02:25:07 -0400

    chore: add Firebase config for hml/prod (android/ios)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_ci_cd_flavors/app/core/config/app_config.dart';
 import 'package:flutter_ci_cd_flavors/app/core/config/app_flavor.dart';
 import 'package:flutter_ci_cd_flavors/app/initialize_app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final flavor = AppFlavor.fromString(AppConfig.environment);
   await initializeApp(flavor);
 }
